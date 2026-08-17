c76cb64f3328f47d2d484196c4cd7ff754bc0efb
Violynnn_ <gatausihb@gmail.com>
2026-08-17 04:39:22 +0000

    Add missing Intent/TextView imports to LauncherActivity

--- app/src/main/java/net/kdt/pojavlaunch/firefly/LauncherActivity.java
+++ app/src/main/java/net/kdt/pojavlaunch/firefly/LauncherActivity.java
@@ -7,12 +7,14 @@ import static net.kdt.pojavlaunch.firefly.prefs.LauncherPreferences.PREF_IGNORE_
 import android.Manifest;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.SwitchCompat;
